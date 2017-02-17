a7a691ebe3d6665e690cf38734d102bed8f613a9
crea <masataka0504@gmail.com>
2017-02-17 13:29:56 -0800

    androidはクソ

--- app/src/main/java/com/example/masa/twitterimageretriever/SimplePreferenceActivity.java
+++ app/src/main/java/com/example/masa/twitterimageretriever/SimplePreferenceActivity.java
@@ -15,13 +15,20 @@ public class SimplePreferenceActivity extends AppCompatActivity {
     private static final String PREF_NAME = "setting";
 
     private static final String TWITTER_ACCOUNT_NAME = "twitter_account_name";
+    private static final String SET_CRAWLER_DURATION = "set_crawler_duration";
+    private static final String GACHA_DURATION = "gacha_duration";
+
+
     private static final String DURATION = "duration";
 
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
 
     EditText et;
-
+    RadioGroup rg1;
+    RadioGroup rg2;
+    //RadioButton rb;
+    //RadioButton rb2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +41,11 @@ public class SimplePreferenceActivity extends AppCompatActivity {
         editor = preferences.edit();
 
         et   = (EditText) findViewById(R.id.editText);
+        rg1  = (RadioGroup) findViewById(R.id.RG1);
+        rg2  = (RadioGroup) findViewById(R.id.RG2);
+        //rb = (RadioButton) findViewById(rg1.getCheckedRadioButtonId());
+        //rb2 = (RadioButton) findViewById(rg2.getCheckedRadioButtonId());
+
 
         findViewById(R.id.OKButton).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,10 +56,8 @@ public class SimplePreferenceActivity extends AppCompatActivity {
                 Context.MODE_PRIVATE);
                 editor = preferences.edit();
 
-                RadioGroup rg = (RadioGroup) findViewById(R.id.RG1);
-                RadioButton rb = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
-
-                RadioGroup rg2 = (RadioGroup) findViewById(R.id.RG2);
+                // 選択されているラジオボタンを定義
+                RadioButton rb = (RadioButton) findViewById(rg1.getCheckedRadioButtonId());
                 RadioButton rb2 = (RadioButton) findViewById(rg2.getCheckedRadioButtonId());
 
                 editor.putString(TWITTER_ACCOUNT_NAME, String.valueOf(et.getText()));
@@ -65,6 +75,7 @@ public class SimplePreferenceActivity extends AppCompatActivity {
             }
         });
 
+
         findViewById(R.id.DismissButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -76,10 +87,40 @@ public class SimplePreferenceActivity extends AppCompatActivity {
 
         // 2回目以降、設定したTwitterアカウントを自動再ロード
         String twitterName = preferences.getString(TWITTER_ACCOUNT_NAME, "No Data");
+        if (twitterName != "No Data") {
+            et.setText(twitterName);
+        }
 
-        if (twitterName == "No Data") {
+
+        String set_crawler_duration = preferences.getString(SET_CRAWLER_DURATION, "No Data");
+        if (set_crawler_duration != "No Data") {
+
+            RadioButton rb = (RadioButton) findViewById(rg1.getCheckedRadioButtonId());
+            RadioButton rb2 = (RadioButton) findViewById(rg2.getCheckedRadioButtonId());
+
+
+//            switch (set_crawler_duration) {
+//                case "1 hour":
+                    rb.setChecked(true);
+//                case "1 day":
+                    //rb.setChecked(true);
+//                case "OFF":
+//                    rb.setChecked(true);
+//            }
+        }
+
+
+
+
+
+//        rget.setText(twitterName);
+
+        String set_gacha_duration = preferences.getString(GACHA_DURATION, "No Data");
+        if (set_gacha_duration == "No Data") {
             return;
         }
-        et.setText(twitterName);
+//        et.setText(twitterName);
+
+
     }
 }
